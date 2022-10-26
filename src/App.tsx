ce760236faec49516633b18d5f2a7181e156aaf0
jessicahowley1 <112496856+jessicahowley1@users.noreply.github.com>
2022-10-26 17:07:04 -0400

    finished 10

--- src/App.tsx
+++ src/App.tsx
@@ -10,56 +10,57 @@ import { Counter } from "./components/Counter";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
-<<<<<<< HEAD
 import { DoubleHalf } from "./bad-components/DoubleHalf";
+import { CheckAnswer } from "./form-components/CheckAnswer";
+import { GiveAttempts } from "./form-components/GiveAttempts";
+import { EditMode } from "./form-components/EditMode";
+import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): JSX.Element {
     return (
-        <>
-            <>
-                <h1>Jessica-Howleys-Header CISC275</h1>
-                <div className="App">
-                    <header className="App-header">
-                        React Hooks and TypeScript- Jess Howley Hello World
-                    </header>
-                    <img
-                        alt="DOG"
-                        src="https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg"
-                    ></img>
-                    <ul id="list 1">
-                        <li>Red</li>
-                        <li>Green</li>
-                        <li>Blue</li>
-                    </ul>
-                    <div>
-                        <Button onClick={() => console.log("Hello World!")}>
-                            Log Hello World
-                        </Button>
-                    </div>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save. This page will
-                        automatically reload Hello World
-                    </p>
-                </div>
-                <div>
-                    <Container>
-                        <Row>
-                            <Col>
-                                First column.
-                                <div>
-                                    width:4px; height:2px; backgroundColor:red;
-                                </div>
-                            </Col>
-                            <Col>
-                                Second column.
-                                <div>
-                                    width:4px; height:2px; backgroundColor:red;
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
-            </>
+        <div>
+            <h1>Jessica-Howleys-Header CISC275</h1>
+            <div className="App"></div>
+            <header className="App-header">
+                React Hooks and TypeScript- Jess Howley Hello World
+            </header>
+            <img
+                alt="DOG"
+                src="https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg"
+            ></img>
+            <ul id="list 1">
+                <li>Red</li>
+                <li>Green</li>
+                <li>Blue</li>
+            </ul>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload Hello World
+            </p>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            First column.
+                            <div>
+                                width:4px; height:2px; backgroundColor:red;
+                            </div>
+                        </Col>
+                        <Col>
+                            Second column.
+                            <div>
+                                width:4px; height:2px; backgroundColor:red;
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             <div className="App">
                 <hr></hr>
                 <Counter></Counter>
@@ -84,56 +85,22 @@ function App(): JSX.Element {
                 <hr></hr>
                 <ShoveBox></ShoveBox>
                 <hr></hr>
+                <div className="App">
+                    <hr></hr>
+                    <CheckAnswer expectedAnswer={""}></CheckAnswer>
+                    <hr></hr>
+                    <GiveAttempts></GiveAttempts>
+                    <hr></hr>
+                    <ChangeColor></ChangeColor>
+                    <hr></hr>
+                    <EditMode></EditMode>
+                    <hr></hr>
+                    {/* <MultipleChoiceQuestion></MultipleChoiceQuestion> */}
+                    <hr></hr>
+                </div>
             </div>
-        </>
-=======
-import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
-
-function App(): JSX.Element {
-    return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <hr></hr>
-            <CheckAnswer expectedAnswer="42"></CheckAnswer>
-            <hr></hr>
-            <GiveAttempts></GiveAttempts>
-            <hr></hr>
-            <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
-            <hr></hr>
-            <MultipleChoiceQuestion
-                options={["a", "b", "c"]}
-                expectedAnswer="b"
-            ></MultipleChoiceQuestion>
-            <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
         </div>
->>>>>>> upstream/task-forms
     );
 }
+
 export default App;
